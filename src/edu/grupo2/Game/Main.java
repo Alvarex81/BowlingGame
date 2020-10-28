6f123af01cef0704442dbd5cc7c03ff268d22639
Alvarex81 <alvarex81@gmail.com>
2020-10-28 14:26:47 -0400

    Initial commit

---
+++ src/edu/grupo2/Game/Main.java
@@ -0,0 +1,21 @@
+package edu.grupo2.Game;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        Game game = new Game();
+        DisplayGame display = new DisplayInConsole();
+        InitiateGame initiator = new InitiateFromFile("src/edu/grupo2/Game/", "Bowling.txt" );
+
+        game.addNewPlayer("Alvarex");
+        game.addRolls("Alvarex", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+
+        initiator.start(game);
+        game.calculatePuntuation();
+        display.showGame (game);
+
+    }
+}
