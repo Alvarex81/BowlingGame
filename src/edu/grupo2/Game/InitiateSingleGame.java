b830fad27d5b84e9b6ac4762375f7f2a517c574d
Alvarex81 <alvarex81@gmail.com>
2020-10-30 09:03:57 -0400

    Modificaciones a clase Line

--- src/edu/grupo2/Game/InitiateSingleGame.java
+++ src/edu/grupo2/Game/InitiateSingleGame.java
@@ -2,5 +2,7 @@ package edu.grupo2.Game;
 
 public class InitiateSingleGame implements InitiateGame {
     @Override
-    public void start(Game game) {}
+    public void start(Game game) {
+        System.out.println("Iniciando un juego sencillo de una sola linea");
+    }
 }
